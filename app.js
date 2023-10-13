b069534511d844f7316ae185fa532864ea074a04
Gvn <gvanca.uplisashvili@gmail.com>
2023-10-13 16:55:06 +0400

    Redirecting requests
    not finished yet

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
   if (url === "/") {
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
@@ -10,6 +12,12 @@ const server = http.createServer((req, res) => {
       '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
     );
     res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.writeHead("Location", "/");
     return res.end();
   }
 });
