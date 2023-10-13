9ddc33198d0e992a4223b2d7bb9607cf96c9866d
Gvn <gvanca.uplisashvili@gmail.com>
2023-10-13 16:39:26 +0400

    Routing requests

---
+++ app.js
@@ -0,0 +1,17 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+});
+
+server.listen(3000);
