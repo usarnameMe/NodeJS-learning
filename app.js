c09496093f3aee1e823f81a1ba7b00ce68a600a9
Gvn <gvanca.uplisashvili@gmail.com>
2023-10-17 14:51:41 +0400

    Express
    handling routes

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-const http = require("http");
 const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware!!!!");
+app.use("/", (req, res, next) => {
+  console.log("This always runs!");
   next();
 });
-app.use((req, res, next) => {
-  res.send("<h1>HELLO!!</>");
+
+app.use("/add-product", (req, res, next) => {
+  console.log("In another middleware!");
+  res.send('<h1>The "Add Product" Page</h1>');
 });
 
-const server = http.createServer(app);
+app.use("/", (req, res, next) => {
+  console.log("In another middleware!");
+  res.send("<h1>Hello from Express!</h1>");
+});
 
-server.listen(3000);
+app.listen(3000);
