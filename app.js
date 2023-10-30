8c416d8f609195f8dc1a87c91c72751b38ad6807
Gvn <gvanca.uplisashvili@gmail.com>
2023-10-30 13:10:00 +0400

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
+});
 app.post("/product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
