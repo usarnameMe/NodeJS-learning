84736549864f4479ba6d59691ba22ed86f268ba2
Gvn <gvanca.uplisashvili@gmail.com>
2023-11-18 12:58:57 +0400

    path
    Learning pathing files.

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const path = require("path")
+
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
 
-app.use(bodyParser.urlencoded({ extended: false }));
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use("/admin", adminRoutes);
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
