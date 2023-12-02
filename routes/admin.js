6557e9d9ed2cedbc8ff2ef7339e16de7bf50aa49
Gvn <gvanca.uplisashvili@gmail.com>
2023-12-02 18:46:27 +0400

    styling
    fixing styling

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,22 @@
 const path = require('path');
+
 const express = require('express');
-const rootDir = require("../util/path");
+
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.render('add-product', { pageTitle: "Add Product", path: "/admin/add-product" }); 
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
 });
 
 // /admin/add-product => POST
