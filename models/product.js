75346dd223c4090c0e05b6855436f63e1b28aac0
Gvn <gvanca.uplisashvili@gmail.com>
2023-12-04 16:03:33 +0400

    .

--- models/product.js
+++ models/product.js
@@ -1,14 +1,34 @@
+const fs = require("fs");
+const path = require("path");
+
 const products = [];
 
 module.exports = class Products {
-    constructor(t) {
-        this.title = t;
-    }
-    save() {
-        products.push(this);
-    };
+  constructor(t) {
+    this.title = t;
+  }
+  save() {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json",
+    );
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        if (err) {
+          // Handle the error here, possibly logging it or informing the user
+          console.log(err);
+        }
+      });
+    });
+  }
 
-    static fetchAll() {
-        return products;
-    }
+  static fetchAll() {
+    return products;
+  }
 };
