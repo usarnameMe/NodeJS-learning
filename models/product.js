0934ea2a7147dafe3881d64a3b448c881c95681a
Gvn <gvanca.uplisashvili@gmail.com>
2023-12-07 00:29:36 +0400

    as

--- models/product.js
+++ models/product.js
@@ -1,12 +1,11 @@
 const fs = require("fs");
 const path = require("path");
 
-const products = [];
-
-module.exports = class Products {
+module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
+
   save() {
     const p = path.join(
       path.dirname(process.mainModule.filename),
@@ -20,21 +19,22 @@ module.exports = class Products {
       }
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
-        if (err) {
-          // Handle the error here, possibly logging it or informing the user
-          console.log(err);
-        }
+        console.log(err);
       });
     });
   }
 
   static fetchAll(cb) {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json",
+    );
     fs.readFile(p, (err, fileContent) => {
       if (err) {
         cb([]);
       }
       cb(JSON.parse(fileContent));
     });
-    // return products;
   }
 };
