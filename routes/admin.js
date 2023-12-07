38b29dfc184eb24b38ce67d965dfee16f500ad5c
Gvn <gvanca.uplisashvili@gmail.com>
2023-12-07 22:51:33 +0400

    .

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,16 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
 const productsController = require("../controllers/products");
 
 const router = express.Router();
 
-
 // /admin/add-product => GET
-router.get('/add-product', productsController.getAddProduct);
+router.get("/add-product", productsController.getAddProduct);
+router.get("/products");
 
 // /admin/add-product => POST
-router.post('/add-product', productsController.postAddProduct);
+router.post("/add-product", productsController.postAddProduct);
 
 module.exports = router;
