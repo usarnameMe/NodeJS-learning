88f4a44c9d187b9c1dc90c522534ed59b87fe4b2
Gvn <122465852+usarnameMe@users.noreply.github.com>
2024-01-12 22:07:23 +0400

    fetch one prod

--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
-const getDb = require('../util/database').getDb;
+const mongodb = require("mongodb");
+const getDb = require("../util/database").getDb;
 
 class Product {
   constructor(title, price, description, imageUrl) {
@@ -11,7 +12,7 @@ class Product {
   save() {
     const db = getDb();
     return db
-      .collection('products')
+      .collection("products")
       .insertOne(this)
       .then(result => {
         console.log(result);
@@ -24,12 +25,27 @@ class Product {
   static fetchAll() {
     const db = getDb();
     return db
-      .collection('products')
+      .collection("products")
       .find()
       .toArray()
       .then(products => {
         console.log(products);
         return products;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  static findById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then(product => {
+        console.log(product);
+        return product;
       })
       .catch(err => {
         console.log(err);
