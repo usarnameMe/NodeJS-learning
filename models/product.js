b7ae2eaa77c770e2b92fb531a5129e4026fc5a4d
Gvn <122465852+usarnameMe@users.noreply.github.com>
2024-01-12 21:34:32 +0400

    fetchAll

--- models/product.js
+++ models/product.js
@@ -1,4 +1,4 @@
-const getDb = require("../util/database").getDb;
+const getDb = require('../util/database').getDb;
 
 class Product {
   constructor(title, price, description, imageUrl) {
@@ -7,40 +7,34 @@ class Product {
     this.description = description;
     this.imageUrl = imageUrl;
   }
+
   save() {
     const db = getDb();
     return db
-    .collection("products") 
-    .insertOne(this)
-      .then((result) => {
+      .collection('products')
+      .insertOne(this)
+      .then(result => {
         console.log(result);
       })
-      .catch((err) => {
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db
+      .collection('products')
+      .find()
+      .toArray()
+      .then(products => {
+        console.log(products);
+        return products;
+      })
+      .catch(err => {
         console.log(err);
       });
   }
 }
-
-// const Product = sequelize.define("product", {
-//   id: {
-//     type: Sequelize.INTEGER,
-//     autoIncrement: true,
-//     allowNull: false,
-//     primaryKey: true,
-//   },
-//   title: Sequelize.STRING,
-//   price: {
-//     type: Sequelize.DOUBLE,
-//     allowNull: false,
-//   },
-//   imageUrl: {
-//     type: Sequelize.STRING,
-//     allowNull: false,
-//   },
-//   description: {
-//     type: Sequelize.STRING,
-//     allowNull: false,
-//   },
-// });
 
 module.exports = Product;
