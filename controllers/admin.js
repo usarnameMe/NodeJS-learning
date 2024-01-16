90817fbd4cc4f2e24601d90f1be5d7a8ce785414
Gvn <122465852+usarnameMe@users.noreply.github.com>
2024-01-16 20:05:24 +0400

    I need to fix getTOCart and getToOrders

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,18 +13,15 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, price, description, imageUrl);
+  const product = new Product(title, imageUrl, description, price);
   product
     .save()
-    .then(result => {
-      // console.log(result);
-      console.log('Created Product');
-      res.redirect('/admin/products');
+    .then(() => {
+      res.redirect('/');
     })
-    .catch(err => {
-      console.log(err);
-    });
+    .catch(err => console.log(err));
 };
+
 
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
--- models/product.js
+++ models/product.js
@@ -2,32 +2,33 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl, id) {
+  constructor(title, price, description, imageUrl, id, userId) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
     this._id = id ? new mongodb.ObjectId(id) : null;
+    this.userId = userId;
   }
 
-  save() {
-    const db = getDb();
-    let dbOp;
-    if (this._id) {
-      // Update the product
-      dbOp = db
-        .collection('products')
-        .updateOne({ _id: this._id }, { $set: this });
-    } else {
-      dbOp = db.collection('products').insertOne(this);
+  async save() {
+    try {
+      const db = getDb();
+      let dbOp;
+
+      if (this._id) {
+        // Update the product
+        dbOp = db.collection('products').updateOne({ _id: this._id }, { $set: this });
+      } else {
+        dbOp = db.collection('products').insertOne(this);
+      }
+
+      const result = await dbOp;
+      return result;
+    } catch (err) {
+      console.error('Error in save method:', err);
+      throw err;  // Rethrow the error for handling at a higher level
     }
-    return dbOp
-      .then(result => {
-        console.log(result);
-      })
-      .catch(err => {
-        console.log(err);
-      });
   }
 
   static fetchAll() {
