0c11b3d8456f642bcba8a4ddef43b5bd4f880eb1
Gvn <122465852+usarnameMe@users.noreply.github.com>
2024-03-26 15:08:47 +0400

    Add pagination! Woohooo!

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
 const crypto = require("crypto");
 
 const bcrypt = require("bcryptjs");
-const { validationResult } = require("express-validator");
+const { validationResult } = require("express-validator/check");
 
 const User = require("../models/user");
 
